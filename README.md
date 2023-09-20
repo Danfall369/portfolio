427f7bc49f171f18ea42b25de847166f50beffc7
Daniel Falcon <Danfall963@Gmail.com>
2023-09-19 22:30:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,163 @@
+<a name="readme-top"></a>
+
+<!-- TABLE OF CONTENTS -->
+
+# 📗 Table of Contents
+
+- [📖 About the Project](#about-project)
+  - [🛠 Built With](#built-with)
+    - [Tech Stack](#tech-stack)
+  - [🚀 Live Demo](#live-demo)
+- [💻 Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Install](#install)
+  - [Usage](#usage)
+- [👥 Author](#author)
+- [🔭 Features](#features)
+- [🤝 Contributing](#contributing)
+- [⭐️ Show your support](#support)
+- [🙏 Acknowledgements](#acknowledgements)
+- [📝 License](#license)
+
+<!-- PROJECT DESCRIPTION -->
+
+# 📖 [My Portfolio] `React.js` `HTML5 & CSS3` `JavaScript` `Style-Components` <a name="about-project"></a>
+
+![My-Portfolio](https://i.imgur.com/LDvs7lB.png)
+
+My Portfolio is a React.js application that serves as an online portfolio to showcase my work, skills, and achievements. It provides an overview of my background, displays my projects, and offers a way to contact me..
+
+## 🛠 Built With <a name="built-with"></a>
+
+### Tech Stack <a name="tech-stack"></a>
+
+<details>
+    <summary>Client</summary>
+    <ul>
+      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript</a></li>
+      <li><a href="https://react.dev/">React</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/HTML5/">HTML 5</a></li>
+      <li><a href="https://en.wikipedia.org/wiki/CSS">CSS 3</a></li>
+    </ul>
+  </details>
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LIVE DEMO -->
+
+## 🚀 Live Demo & preview <a name="live-demo"></a>
+
+- ![Skills](https://i.imgur.com/6nA9tPN.png)
+- ![experience](https://i.imgur.com/I2F3xn2.png)
+- ![Projects](https://i.imgur.com/v1QyoWl.png)
+- ![Education](https://i.imgur.com/csfs4Ce.png)
+- ![Contac](https://i.imgur.com/wfGKXGd.png)
+
+- _You can visit the live demo [here](https://)_
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- GETTING STARTED -->
+
+## 💻 Getting Started <a name="getting-started"></a>
+
+To get a local copy up and running, follow these steps.
+
+### Prerequisites
+
+In order to run this project you need:
+
+```sh
+ To have a computer, Internet, Keyboard and Mouse
+```
+
+### Install
+
+Clone this repository to your desired folder:
+
+```sh
+  git clone https://github.com/danfall369/danfall-portfolio.git
+```
+
+Navigate to the project directory:
+
+```sh
+  cd danfall-portfolio
+```
+
+Install the dependencies:
+
+```sh
+  npm install
+```
+
+Start the development server:
+
+```sh
+  npm start
+```
+
+Open your browser and visit:
+
+```sh
+  http://localhost:3000
+```
+
+### Usage
+
+After installing and running the project locally, you can navigate through the different sections of the portfolio using the navigation menu. Explore the About Me section to learn more about my background and skills. Visit the Projects section to see detailed information about my projects, including descriptions and screenshots. Use the Contact section to send me a message or inquiry.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- AUTHOR -->
+
+## 👥 Authors <a name="author"></a>
+
+- GitHub: [Danfall369](https://github.com/Danfall369)
+- LinkedIn: [Danfall369](https://www.linkedin.com/in/danfall/)
+
+<!-- FEATURES -->
+
+## 🔭 Features <a name="features"></a>
+
+- **[About me]** An overview of my background, skills, and experiences.
+- **[Projects]** A collection of my notable projects with descriptions, screenshots, and links.
+- **[Skills]** A list of my technical skills and proficiencies.
+- **[Resume]** Resume.A link to download my resume.
+- **[Contact]** A form to send me messages or inquiries.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTRIBUTING -->
+
+## 🤝 Contributing <a name="contributing"></a>
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- SUPPORT -->
+
+## ⭐️ Show your support <a name="support"></a>
+
+If you like this project, be pending on our profiles since we'll be doing much more!
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- ACKNOWLEDGEMENTS -->
+
+## 🙏 Acknowledgments <a name="acknowledgements"></a>
+
+To [Rishav Chanda](https://github.com/rishavchanda) for sharing his [template](https://github.com/rishavchanda/rishavchanda.github.io) for this project, take a look at his projects and give him some stars!
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- LICENSE -->
+
+## 📝 License <a name="license"></a>
+
+This project is [MIT](./LICENSE) licensed.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ src/data/constants.js
@@ -0,0 +1,314 @@
+export const Bio = {
+  name: "Daniel Falcon",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a versatile web developer with expertise in Ruby on Rails, React, JavaScript, CSS, and database management with PostgreSQL. Beyond technical proficiency, my effective communication, collaboration, and rapid skill acquisition enable me to excel in dynamic teams, whether in front-end or back-end roles.",
+  github: "https://github.com/Danfall369",
+  resume:
+    "https://drive.google.com/file/d/1PeSsZjVHFyk01vOmCwFxnd7BMB5vfOyH/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/danfall369/",
+  twitter: "https://twitter.com/Danfall369",
+  insta: "https://www.instagram.com/danfall369/",
+  facebook: "https://www.facebook.com/Danfall369",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Next Js",
+        image:
+          "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAACTklEQVR4Ab1XAaQqURB9DyohSykREpRIQSAlBCoECKUFCSRCBBEAaSEABQEoCIEASCwAUICALgCo83do0//9v819XX845O7VnDkzOzP7JWGaBd3C3IJpQVjAHeJ+Rs9a97vKLGrBsB1KgMhEP3FMUUwt4ENMfxr1yQIU4SSjRkbeOZtERmHk6pXQVDlnkHh9S+QLTm1hkiz4n/gzFQuny9FoFLquE+i34x+n02k0m00UCoV3BIzn3MMJrVYLtp1OJ0cS/X4f5/MZhmG8IyDsWtDfEaDIn2232/3zbrvdxuFwwGg04qRBt+VnETBNE0IIkE2n07/erdfrWK/X6Ha73Hb9ZXII3G43ivy3dNRqtZe7lUoFs9mM6oBDwCQCgquALT1FT3a5XF7qIZ/PYzgcolqtcggIIgBZAgRKB6lCRalp2uM8k8mAVMrlchwC+DEBipycE4n5fP44j8ViKJVKSCaTbAJCpgaez4vFIsjoWa/XA50FAgEkEgmEw2F2CkxZBZ5Br5tt1ITcbjd8Ph88Hg+7CBefECCsVitS4aVJcV9D/VMCVITk/Hq9YrPZyBBo2a1YMGvAcQYcj0cCtWMugcdYNhjDiBrP25mx3++x3W6RzWZZ8isfxzQLlsslJpMJpYY5jhkqcOH1ejEYDDAej9FoNOByuZxGsfqVzC7KTqcDSkkqleKsZOqX0mAwiHK5DGrJfr+fs5SqX8sjkQji8ThCoRC+v78Za7l6JagrUh3YkUuZpqgwDaecc9VYSDoV5Fg+at7n+eLN57kuE/EvzHr/Kvs31aYAAAAASUVORK5CYII=",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Ruby",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/800px-Ruby_logo.svg.png",
+      },
+      {
+        name: "Ruby on Rails",
+        image:
+          "https://www.svgrepo.com/show/376345/rails.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "Sanity",
+        image: "https://www.sanity.io/static/images/logo_rounded_square.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Linux",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/f/f1/Icons8_flat_linux.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://seeklogo.com/images/N/netlify-logo-BD8F8A77E2-seeklogo.com.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://scontent.fccs8-1.fna.fbcdn.net/v/t39.30808-6/305751749_449624627270583_2289770462671144705_n.png?_nc_cat=105&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=Ch1GyqfYn5AAX-qVFZH&_nc_ht=scontent.fccs8-1.fna&oh=00_AfA3jyRA_7cJW14cl9DNEzDMO4takWo56W-GQlvwQuv2KA&oe=650E621C",
+    role: "Mentor (Volunteer)",
+    company: "MICROVERSE",
+    date: "Apr 2023 - Sep 2023",
+    desc: "Mentored juniors web developers, providing technical support through codes reviews, Proposed improvements to code organization to improve code quality and overall performance and Provided advice and tips on how to maintain motivation to maintain longevity in the program.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Eslint",
+      "Bootstrap",
+      "React",
+      "Redux",
+      "Jest",
+      "Ruby",
+      "Ruby on Rails",
+      "Rubocop",
+      "GitHub",
+      "Zoom",
+    ],
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://scontent.fccs8-1.fna.fbcdn.net/v/t39.30808-6/305751749_449624627270583_2289770462671144705_n.png?_nc_cat=105&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=Ch1GyqfYn5AAX-qVFZH&_nc_ht=scontent.fccs8-1.fna&oh=00_AfA3jyRA_7cJW14cl9DNEzDMO4takWo56W-GQlvwQuv2KA&oe=650E621C",
+    school: "MICROVERSE",
+    date: "Feb 2023 - Sep 2023",
+    desc: "Spends 1300+ hours mastering algorithms, data structures, and full-stack development while simultaneously developing projects with Ruby, Rails, JavaScript, React, and Redux and Developing skills in remote pair programming using GitHub, industry-standard git-flow, and daily standups to communicate and collaborate with international remote developers.",
+    degree: "Full Stack Web Development Program",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Land Rover E-commerce",
+    date: "Aug 2023",
+    description:
+      "A cutting-edge website that's more than just your typical e-commerce platform. At its core, it's a dynamic hub driven by requests and API calls. What sets us apart? We've custom-built our robust backend using Ruby on Rails, complete with top-notch authentication and authorization features.",
+    image:
+      "https://i.imgur.com/KJPNrxe.png",
+    tags: [
+      "Full-Stack App",
+      "React Js",
+      "Redux",
+      "Netlify",
+      "User Auth",
+      "UI/UX",
+      "API Rest",
+      "Axiox",
+      "Ruby on Rails",
+      "Devise",
+      "CanCanCan",
+      "PostgreSLQ",
+      "Database",
+      "E-Commerce",
+    ],
+    category: "web app",
+    github: "https://github.com/VelzckC0D3/Land-Rover-E-Commerce",
+    webapp: "https://land-rover-e.netlify.app/vehicles",
+  },
+  {
+    id: 0,
+    title: "Budget It!",
+    date: "May 2023",
+    description:
+      "Budget it! app is a mobile web application made in Ruby on Rails, using PostgreSQL as database storage, whit manages user authentication with Devise and authorization with CanCanCan. Where you can manage your budget: you have a list of transactions associated with a category so that you can see how much money you spent and on what.",
+    image:
+      "https://i.imgur.com/gdgjd6o.png",
+    tags: ["Ruby on Rails", "Database", "PostgreSQL", "Devise", "CanCanCan", "Rubocop", "Mobile"],
+    category: "android app",
+    github: "https://github.com/Danfall369/BudgetIt",
+    webapp: "https://github.com/Danfall369/BudgetIt",
+  },
+  {
+    id: 1,
+    title: "Space Travellers",
+    date: "Aug 2023",
+    description:
+      "This is a user-friendly web app that lets you explore a variety of rockets and missions for your space travel aspirations. With seamless integration of React and Redux, it fetches data from the Space X API, providing a wide range of rocket and mission options. Your selected choices are stored persistently using browser local storage, ensuring easy retrieval and review. Experience the thrill of selecting your dream rocket and mission.",
+    image:
+      "https://media.licdn.com/dms/image/D4E2DAQHA4Oh3QPn_xQ/profile-treasury-image-shrink_800_800/0/1692608794149?e=1695769200&v=beta&t=nJpnQmMz2zLHsr2YdXaWJRLJAimT1J6RpqZwwqGFeSU",
+    tags: [
+      "JavaScript",
+      "React JS",
+      "React-Testing-Library",
+      "React-Router",
+      "Redux",
+      "API Rest",
+      "Axios",
+      "Eslint",
+    ],
+    category: "web app",
+    github: "https://github.com/Danfall369/Space-Travelers-Hub",
+    webapp: "https://spaceship-missions.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Grocery Store",
+    date: "jun 2023",
+    description:
+      "innovative e-commerce platform built using React, featuring a seamless shopping experience. This project leverages the power of React's component-based architecture, utilizing props to efficiently manage states and interactions. Users can explore a diverse range of products and manage their carts.",
+    image:
+      "https://i.imgur.com/kbZkhiw.png",
+    tags: ["Full-Stack App", "JavaScript", "React JS", "Sanity", "Stripe", "CSS", "React-Dom", "Mobile", "Responsive", "E-Commerce"],
+    category: "web app",
+    github: "https://github.com/Danfall369/grosery-store",
+    webapp: "https://multitienda-eaf.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Blog-app",
+    date: "Jul 2023",
+    description:
+      "is a comprehensive blog application that offers users the ability to create, edit, and delete their posts, interact with other users' posts through comments, and manage their profiles. This project utilizes the Devise gem for authentication and the CanCanCan gem for authorization, providing a secure and user-friendly experience.",
+    image:
+      "https://media.licdn.com/dms/image/D4E2DAQE8BfkIynKhmg/profile-treasury-image-shrink_800_800/0/1692608265990?e=1695772800&v=beta&t=oXkNEIzQFqxbmd2ImpXP951ppy-KB0aFJqD-yW5xIqA",
+    tags: ["Ruby on Rails", "CSS", "Database", "PostgreSQL", "Devise", "CanCanCan", "UI/UX", "User Auth"],
+    category: "web app",
+    github: "https://github.com/Danfall369/Blog-app",
+    webapp: "https://github.com/Danfall369/Blog-app",
+  },
+  {
+    id: 10,
+    title: "Catalog of my things",
+    date: "Jul 2023",
+    description:
+      "This project involves developing a console application designed to assist users in maintaining a comprehensive record of various personal items such as books, music albums, and games. The entire system will be structured around a UML class diagram, ensuring a well-organized representation. The data will be stored in JSON files while also offering the option of utilizing a SQL database model for enhanced functionality and scalability.",
+    image:
+      "https://media.licdn.com/dms/image/D4E2DAQFptBaGIuXBBw/profile-treasury-image-shrink_1920_1920/0/1692607250238?e=1695772800&v=beta&t=HZmCSgTOyYMwv9SIXYQrXWVagPFLw-tpmq44MVomlE4",
+    tags: ["Ruby", "Terminal App", "Json", "Data Storage"],
+    category: "machine learning",
+    github: "https://github.com/Danfall369/Catalog-of-my-things",
+    webapp: "https://github.com/Danfall369/Catalog-of-my-things",
+  },
+  {
+    id: 4,
+    title: "D'Fall Store",
+    date: "May 2023",
+    description:
+      "is an React/Redux immersive e-commerce web application that leverages the power of React and RESTful API requests to create a seamless shopping experience. This project showcases the development of a user-friendly interface for browsing and purchasing products from various categories, all while maintaining a minimalistic and responsive design.",
+    image:
+      "https://i.imgur.com/471Mjev.png",
+    tags: ["JavaScript", "React Js", "API Rest", "Axios", "Redux", "CSS", "Mobile", "Responsive", "E-Commerce"],
+    category: "android app",
+    github: "https://github.com/Danfall369/metrics-webapp",
+    webapp: "https://dfall-store.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Food Truck",
+    date: "Apr 2023",
+    description:
+      " is a recipe search web app that allows users to find and save recipes based on their preferences. The app is built using JavaScript and utilizes ES6 syntax and modules to organize the codebase into smaller, reusable components. It follows best practices and language style guides to ensure code quality and maintainability. Webpack is used to bundle the application and its dependencies.",
+    image:
+      "https://i.imgur.com/wTruBG9.png",
+    tags: ["HTML", "CSS", "JavaScrip", "API Rest", "Axios", "Local Storage"],
+    category: "web app",
+    github: "https://github.com/J-C-S-V/capstone-project-module-2",
+    webapp: "https://development--friendly-strudel-5e52a8.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "Criminal Investigation Seminar",
+    date: "Mar 2023",
+    description:
+      "is a dynamic and immersive web page designed to host an engaging seminar focused on criminalistics. The project showcases a seamless integration of HTML, CSS, and JavaScript to create an interactive and visually appealing platform for attendees.",
+    image:
+      "https://i.imgur.com/E6jOgzV.png",
+    tags: ["HTML", "CSS", "JavaScrip", "Mobile", "Responsive", "ESlint"],
+    category: "web app",
+    github: "https://github.com/Danfall369/HTML-CSS-Capstone",
+    webapp: "https://danfall369.github.io/HTML-CSS-Capstone/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
