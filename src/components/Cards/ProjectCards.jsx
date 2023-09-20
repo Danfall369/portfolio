ac3f6cc127e0f77ed61846fd9150aa7217b70ad4
Daniel Falcon <Danfall963@Gmail.com>
2023-09-20 15:30:16 -0400

    fixes to the page

--- src/components/Cards/ProjectCards.jsx
+++ src/components/Cards/ProjectCards.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 
-
-const Button = styled.button`
-    display: none;
+const Button = styled.a`
     width: 100%;
-    padding: 10px;
-    background-color: ${({ theme }) => theme.white};
-    color: ${({ theme }) => theme.text_black};
-    font-size: 14px;
-    font-weight: 700;
-    border: none;
-    border-radius: 10px;
+    text-align: center;
+    font-size: 16px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text_primary};
+    padding: 12px 16px;
+    border-radius: 8px;
+    background-color: ${({ theme }) => theme.primary};
+    ${({ dull, theme }) => dull && `
+        background-color: ${theme.bgLight};
+        color: ${theme.text_secondary};
+        &:hover {
+            background-color: ${({ theme }) => theme.bg + 99};
+        }
+    `}
     cursor: pointer;
-    transition: all 0.8s ease-in-out;
-`
+    text-decoration: none;
+    transition: all 0.5s ease;
+    &:hover {
+        background-color: ${({ theme }) => theme.primary + 99};
+    }
+    @media only screen and (max-width: 600px) {
+        font-size: 12px;
+    }
+`;
+
 const Card = styled.div`
     width: 330px;
     height: 490px;
@@ -26,7 +39,9 @@ const Card = styled.div`
     padding: 26px 20px;
     display: flex;
     flex-direction: column;
-    gap: 14px;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
     transition: all 0.5s ease-in-out;
     &:hover {
         transform: translateY(-10px);
@@ -94,18 +109,12 @@ const Date = styled.div`
     }
 `
 
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    gap: 20px;
+`;
 
-const Description = styled.div`
-    font-weight: 400;
-    color: ${({ theme }) => theme.text_secondary + 99};
-    overflow: hidden;
-    margin-top: 8px;
-    display: -webkit-box;
-    max-width: 100%;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    text-overflow: ellipsis;
-`
 
 const ProjectCards = ({ project, setOpenModal }) => {
     return (
@@ -119,9 +128,11 @@ const ProjectCards = ({ project, setOpenModal }) => {
             <Details>
                 <Title>{project.title}</Title>
                 <Date>{project.date}</Date>
-                <Description>{project.description}</Description>
             </Details>
-            {/* <Button>View Project</Button> */}
+            <ButtonGroup>
+                <Button dull href={project?.github} target='new'>View Code</Button>
+                <Button href={project?.webapp} target='new'>View Live App</Button>
+            </ButtonGroup>
         </Card>
     )
 }
